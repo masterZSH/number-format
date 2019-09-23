52b570aac8787012f2f4110d2eb1a6f332f91814
zsh <shaohua.zhou@joojtech.com>
2019-09-23 10:54:13 +0800

    A:add typescript suports

--- README.md
+++ README.md
@@ -13,6 +13,25 @@ a number format package
 | type | One of ['thousand', 'lakh', 'wan'] |thousand| Define the thousand grouping style, It support three types. thousand style (thousand) : `123,456,789`, indian style (lakh) : `12,34,56,789`, chinese style (wan) : `1,2345,6789`|
 | separator | separator | , | Sets the thousands/wan/laxh separator. |
 
+### import
+
+```js
+// es6 module
+import numberFormat from 'z-number-format';
+
+// node module
+var numberFormat = require('z-number-format');
+
+// cdn Available on unpkg or 74b
+<script src="https://unpkg.com/z-number-format@1.1/src/index.js"></script> 
+
+<script src="https://74b.cc/js/zNumberFormat.js"></script> 
+```
+
+### typescript
+supports typescript
+
+
 ### Examples
 ```js
 var numberFormat = require('z-number-format');
--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-        typeof define === 'function' && define.amd ? define(factory) :
-            (global = global || self, global.Vue = factory());
-}
-(this, function () {
-    'use strict';
-    const thousandsGroupRegex = {
-        "lakh": /(?<!\d*\.\d*)(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/g,
-        "wan":/(?<!\d*\.\d*)(\d)(?=(\d{4})+(?!\d))/g,
-        "thousand":/(?<!\d*\.\d*)(\d)(?=(\d{3})+(?!\d))/g
-    };
+        typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+            typeof define === 'function' && define.amd ? define(factory) :
+            (global = global || self, global.numberFormat = factory());
+    }
+    (this, function () {
+        'use strict';
+        const thousandsGroupRegex = {
+            "lakh": /(?<!\d*\.\d*)(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/g,
+            "wan": /(?<!\d*\.\d*)(\d)(?=(\d{4})+(?!\d))/g,
+            "thousand": /(?<!\d*\.\d*)(\d)(?=(\d{3})+(?!\d))/g
+        };
 
-    /**
-     * Add thousand separators on number
-     * Define the thousand grouping style, It support three types.
-     * thousand style (thousand) : 123,456,789,
-     * indian style (lakh) : 12,34,56,789,
-     * chinese style (wan) : 1,2345,6789
-     * @param number
-     * @param type -One of thousandsGroupRegex ['thousand', 'lakh', 'wan']
-     * @param separator
-     */
-    function numberFormat(number,type="thousand",separator=","){
-        let numberType = typeof number;
-        if(numberType !== 'string' && numberType !== 'number' )
-        {
-            throw  new TypeError("number must be a string or number");
-        }
-        if(-1 === Object.keys(thousandsGroupRegex).indexOf(type)){
-            throw  new RangeError("type must One of thousandsGroupRegex ['thousand', 'lakh', 'wan']")
-        }
-        number = String(number);
-        // console.log(number,thousandsGroupRegex[type],"$1"+separator);
-        return number.replace(thousandsGroupRegex[type],"$1"+separator);
-    };
+        /**
+         * Add thousand separators on number
+         * Define the thousand grouping style, It support three types.
+         * thousand style (thousand) : 123,456,789,
+         * indian style (lakh) : 12,34,56,789,
+         * chinese style (wan) : 1,2345,6789
+         * @param {inputNumber} number -which need to format
+         * @param {string} type -One of thousandsGroupRegex ['thousand', 'lakh', 'wan']
+         * @param {string} separator -separator  
+         * @returns {string}
+         */
+        function numberFormat(number, type = "thousand", separator = ",") {
+            let numberType = typeof number;
+            if (numberType !== 'string' && numberType !== 'number') {
+                throw new TypeError("number must be a string or number");
+            }
+            if (-1 === Object.keys(thousandsGroupRegex).indexOf(type)) {
+                throw new RangeError("type must One of thousandsGroupRegex ['thousand', 'lakh', 'wan']")
+            }
+            number = String(number);
+            // console.log(number,thousandsGroupRegex[type],"$1"+separator);
+            return number.replace(thousandsGroupRegex[type], "$1" + separator);
+        };
 
-    //    exposed public method
-    return numberFormat;
-}));
+        //    exposed public method
+        return numberFormat;
+    })
+);
